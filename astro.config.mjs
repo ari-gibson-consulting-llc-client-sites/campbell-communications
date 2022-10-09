5f5dced71191f19c9bc7c3669a79f96c85a3a205
Ari Gibson <hi@arimgibson.com>
2022-10-08 23:48:39 -0600

    Done for the night

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,25 @@ export default defineConfig({
     plugins: [
       unocss({
         transformers: [transformerDirectives()],
-        presets: [presetUno(), presetDaisy()],
+        presets: [presetUno(), presetDaisy({})],
+        rules: [["list-dash", { "list-style-type": "'-' " }]],
+        theme: {
+          colors: {
+            cc: {
+              base: {
+                100: "#D2DAD6",
+                200: "#A6B4B0",
+                300: "#7A8F8E",
+                400: "#4E6168",
+                500: "#233142",
+                600: "#1E253A",
+                700: "#191A32",
+                800: "#19152A",
+                900: "#181022",
+              },
+            },
+          },
+        },
       }),
     ],
   },
