4d5fe18c1f8773b004c2a94aaa4b522fda8a8008
Ari Gibson <hi@arimgibson.com>
2022-11-15 13:59:36 -0700

    Updated experience, removed hardcoded data

---
+++ src/data/experience.ts
@@ -0,0 +1,41 @@
+interface Project {
+  title: string;
+  desc: string;
+}
+
+const projects: Array<Project> = [
+  {
+    title: "Norwest Banks",
+    desc: "Created a complete marketing plan to help Norwest (today Wells Fargo) become the premier bank in an area in Nebraska",
+  },
+  {
+    title: "Boulder Rotary Club",
+    desc: 'Wrote the forward for two books published by the group: one of which was a recipe book during the pandemic, the other was a collection of art and poems from local 9-year-olds responding to the prompt "What is Peace"',
+  },
+  {
+    title: "Henry Doorly Zoo",
+    desc: "Completed various writing pieces including their guide book, fundrasing materials, and membership events brochures",
+  },
+  {
+    title: "Western Disposal",
+    desc: "Created promotional to help students gain insight into preservation and pollution control",
+  },
+  {
+    title: "Marriott Hotel",
+    desc: "Planned opening for Nebraska's first Marriott hotel and continued producing promotional materials with the hotel's marketing director",
+  },
+  {
+    title: "United States Holocaust Memorial Museum",
+    desc: "Headed fundraising in Nebraska for creation of national Holocaust Memorial Museum, including the planning and presentation of fundraising events",
+  },
+  {
+    title: "Majers Corporation",
+    desc: "Created newspaper articles and ad copy and the planning and organization of a trip to Bermuda honoring top personnel. Wrote a memory book for those attending",
+  },
+  {
+    title: "Other",
+    desc: "Produced numerous other plans, public relations, and marketing plans for a variety of companies, individuals, and organizations during her PR, editing, and advertising career",
+  },
+];
+
+export default projects;
